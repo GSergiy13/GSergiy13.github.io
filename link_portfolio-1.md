ed8a8e019bdeab10175eb1c28148e597b1186105
Serhiy Gerych <frodo13lot@gmail.com>
2024-07-20 20:40:51 +0300

    update portfolio desing

---
+++ link_portfolio-1.md
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+
+
+
+
+https://lala-stars.com/
+https://gart.com.ua/
+
+https://powercodquiz.site/salesin-it/
+https://powercodquiz.site/design-booster/
+
--- page-portfolio/YS/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
- 	$('.s19__tit').on('click', function () {
- 		$(this).toggleClass('active').siblings('ul').slideToggle()
- 	})
-
- 	$('.carousel').carousel()
-
-
-});
